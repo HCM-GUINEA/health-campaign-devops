ec9df46358af06a103d4ecb823fb0c054e4d50b6
goyaldivij-egov <divij.goyal@equidhi.org>
2025-07-25 18:00:56 +0530

    infra creation for guinea-uat

---
+++ infra-as-code/terraform/azure-guinea-uat/variables.tf
@@ -0,0 +1,46 @@
+variable "environment" {
+  default = "azure-guinea-uat"
+}
+variable "resource_group" {
+  default = "azure-guinea-uat"
+}
+
+variable "location" {
+  default = "South Africa North"
+}
+
+variable "db_version" {
+    default = "15"
+}
+
+variable "db_user" {
+    default = "guineahealthuat"
+}
+
+variable "db_password"{}
+variable "subscription_id" {
+  description = "The Subscription ID for Azure"
+  type        = string
+  default = "19026fcf-0fbf-4161-b372-03030cfe66fb"
+}
+
+variable "kubernetes_version" {
+  default = "1.31"
+}
+
+# variable "tenant_id" {
+#   description = "The Tenant ID for Azure Active Directory"
+#   type        = string
+#   default = "b80c308c-d08d-4b07-915c-11a92d9cc6bd"
+# }
+
+variable "client_id" {
+  description = "The Client ID for Azure Active Directory Application"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "The Client Secret for Azure Active Directory Application"
+  type        = string
+  sensitive   = true
+}
